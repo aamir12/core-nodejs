82d6aaf8fdcf582e8f716d234a91579cc7ccba4f
aamir12 <aamirkhan8878@gmail.com>
2024-05-05 20:23:44 +0530

    update

--- 3-module-caching/index.js
+++ 3-module-caching/index.js
@@ -10,3 +10,13 @@ console.log(newSuperHero.getName());
  * We can see that in both case, the name is 'SuperMan'. 
  * It is because when module caching. When we import same file multiple times, it will return same object. Eg: super-hero.js is imported 2 times
  */
+
+/**
+ * Better code : For reuseablity purpose; Here I altered the code inside reusable.js.
+ */
+
+const SuperHero = require('./reusable');
+const hero1 = new SuperHero('Hero1');
+console.log(hero1.getName());
+const hero2 = new SuperHero('Hero2');
+console.log(hero2.getName())
